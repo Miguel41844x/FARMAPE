2b367b5dab0a34602d56425be2026296e0139f6f
Miguel41844x <miguel.sanchezo@unmsm.edu.pe>
2026-07-12 01:01:44 -0500

    Banners

--- backend-microservices/farmape-ms-config/src/test/java/com/farmape/ms/config/ApplicationTests.java
+++ backend-microservices/farmape-ms-config/src/test/java/com/farmape/ms/config/ApplicationTests.java
@@ -1,5 +1,8 @@
 package com.farmape.ms.config;
 
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,6 +20,22 @@ class ApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void hasSpringBootBanner() throws Exception {
+        try (InputStream input = getClass().getResourceAsStream("/banner.txt")) {
+            assertThat(input).isNotNull();
+
+            String banner = new String(input.readAllBytes(), StandardCharsets.UTF_8);
+
+            assertThat(banner)
+                    .hasSizeGreaterThan(100)
+                    .contains("${application.title}")
+                    .contains("${application.version}")
+                    .contains("Powered by Spring Boot")
+                    .contains("${spring-boot.version}");
+        }
     }
 
     @Test
--- backend-microservices/farmape-ms-gateway/src/test/java/com/farmape/ms/gateway/ApplicationTests.java
+++ backend-microservices/farmape-ms-gateway/src/test/java/com/farmape/ms/gateway/ApplicationTests.java
@@ -1,7 +1,12 @@
 package com.farmape.ms.gateway;
 
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(properties = {
         "spring.config.import=optional:configserver:",
@@ -13,5 +18,21 @@ class ApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void hasSpringBootBanner() throws Exception {
+        try (InputStream input = getClass().getResourceAsStream("/banner.txt")) {
+            assertThat(input).isNotNull();
+
+            String banner = new String(input.readAllBytes(), StandardCharsets.UTF_8);
+
+            assertThat(banner)
+                    .hasSizeGreaterThan(100)
+                    .contains("${application.title}")
+                    .contains("${application.version}")
+                    .contains("Powered by Spring Boot")
+                    .contains("${spring-boot.version}");
+        }
     }
 }
