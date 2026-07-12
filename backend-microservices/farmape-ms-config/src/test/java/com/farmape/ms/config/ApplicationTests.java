9de3935f43ccf22ff542d4a48c664f71a762a949
Miguel41844x <miguel.sanchezo@unmsm.edu.pe>
2026-07-12 00:52:18 -0500

    Verificación del test

--- backend-microservices/farmape-ms-config/src/test/java/com/farmape/ms/config/ApplicationTests.java
+++ backend-microservices/farmape-ms-config/src/test/java/com/farmape/ms/config/ApplicationTests.java
@@ -1,12 +1,50 @@
 package com.farmape.ms.config;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.config.environment.Environment;
+import org.springframework.cloud.config.environment.PropertySource;
+import org.springframework.cloud.config.server.environment.EnvironmentRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ApplicationTests {
 
+    @Autowired
+    private EnvironmentRepository environmentRepository;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void publishesEurekaConfiguration() {
+        Environment environment = environmentRepository.findOne("farmape-ms-eureka", "default", null);
+
+        assertThat(environment.getPropertySources())
+                .extracting(PropertySource::getSource)
+                .anySatisfy(source -> {
+                    assertThat(source.get("spring.application.name")).isEqualTo("farmape-ms-eureka");
+                    assertThat(source.get("server.port")).isEqualTo(8761);
+                });
+        assertThat(environment.toString())
+                .contains("farmape-ms-eureka")
+                .contains("eureka");
+    }
+
+    @Test
+    void publishesGatewayConfiguration() {
+        Environment environment = environmentRepository.findOne("farmape-ms-gateway", "default", null);
+
+        assertThat(environment.getPropertySources())
+                .extracting(PropertySource::getSource)
+                .anySatisfy(source -> {
+                    assertThat(source.get("spring.application.name")).isEqualTo("farmape-ms-gateway");
+                    assertThat(source.get("server.port")).isEqualTo(8080);
+                });
+        assertThat(environment.toString())
+                .contains("farmape-ms-gateway");
     }
 }
